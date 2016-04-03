2da3ed120e75465f4098098d10751ba44db86738
Neil <njoshi22@icloud.com>
2016-04-03 17:33:49 +0530

    version 0.1
    chat queue management init

---
+++ index.js
@@ -0,0 +1,44 @@
+var Firebase = require('firebase');
+var Queue = require('firebase-queue');
+
+var ref = new Firebase('https://nj-react.firebaseio.com');
+var queueRef = ref.child('messageQueue');
+
+var q = new Queue(queueRef, function(data,progress,resolve,reject) {
+	var user = data.to;
+	var userRef = new Firebase('https://nj-react.firebaseio.com/' + user);
+	var userMessages = userRef.child('messages');
+	var userStatus = userRef.child('data');
+
+	userStatus.on('value', function(snap) {
+		userMessages.push(transformMessage(snap.val().isAvailable, data), 
+			function(error) {
+				if(error) {
+					console.log(error);
+					reject(error);
+				} else {
+					resolve(data);
+				}
+		});
+	});
+});
+
+function transformMessage(userStatus, message) {
+	if(userStatus == true) {
+		return {
+			to: message.to,
+			from: message.from,
+			text: message.text,
+			time: message.time,
+			serverTime: Firebase.ServerValue.TIMESTAMP
+		};
+	}
+
+	return {
+		to: message.to,
+		from: 'Firebase Queue',
+		text: message.text,
+		time: message.time,
+		serverTime: Firebase.ServerValue.TIMESTAMP
+	};
+};

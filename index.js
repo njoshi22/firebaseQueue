227124b9fdb9c5a96bbc81f8df88ad4da123f47a
Neil <njoshi22@icloud.com>
2016-04-03 18:10:20 +0530

    Fix again

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var q = new Queue(queueRef, function(data,progress,resolve,reject) {
 });
 
 function transformMessage(userStatus, message) {
-	if(userStatus) {
+	if(userStatus == 'true') {
 		return {
 			to: message.to,
 			from: message.from,
